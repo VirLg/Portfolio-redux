4399ed2f91826246095c29ae1c5bd0f9252381ef
VirLg <108211942+VirLg@users.noreply.github.com>
2023-08-16 15:50:12 +0300

    fix Home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,26 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import Api from 'api/Api';
+import React, { useEffect } from 'react';
+
 import HomePageItem from './HomePageItem';
 import { getAllMovieThunk } from 'components/redux/thunk';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectorAllMovie } from 'components/redux/selector';
-// import HomePageItem from './HomePageItem';
+
 const HomePage = () => {
-  // const [arrMovies, setArrMovies] = useState([]);
+
 const dispatch = useDispatch()
 const arrMovies = useSelector(selectorAllMovie)
-console.log('arrMovies', arrMovies)
+
   useEffect(() => {
   dispatch(getAllMovieThunk())
-    // let isCancelled = false;
-    // if (!isCancelled) handleApi();
-    // return () => (isCancelled = true);
-  }, []);
-  // const handleApi = async () => {
-  //   getAllMovieThunk()
-  //   // const arr = await Api(`/trending/movie/day?`);
-  //   // setArrMovies(arr.data.results);
-  // };
+
+  }, [dispatch]);
+
   return (
     <>
       <HomePageItem prop={arrMovies} />
