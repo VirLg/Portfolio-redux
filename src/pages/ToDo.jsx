46b44072f88929f38736fd7fedee1bd80bd55c0d
VirLg <19virlg@gmail.com>
2023-09-14 15:19:01 +0300

    add

--- src/pages/ToDo.jsx
+++ src/pages/ToDo.jsx
@@ -1,7 +1,15 @@
+import ToDoForm from 'components/toDo/ToDoForm';
 import React from 'react';
 
 const ToDo = () => {
-  return <div>ToDo</div>;
+  const formSubmit = prop => {
+    console.log('props', prop);
+  };
+  return (
+    <div>
+      <ToDoForm formSubmit={formSubmit} />
+    </div>
+  );
 };
 
 export default ToDo;
